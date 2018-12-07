975c928462ed2fe2cd29fdf94b5181bf3f52bd71
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-07 07:25:17 +0000

    Updating

--- translate.sh
+++ translate.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=../key.json
-find content -iname "2018-10-2**[^.]??.markdown" |
-while read filename
-do 
-  node translate.js -s $filename -t hi,fr,es,ja,de,vi,ru,id  \;
-done
+
+node translate.js -s $1 -t hi,fr,es,ja,de,vi,ru,id  \;
 
