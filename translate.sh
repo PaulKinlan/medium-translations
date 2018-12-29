7d9af1e8c53f02f95a58c716aa1661ddefd92064
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 18:47:12 +0000

    First stage of translation per branch

--- translate.sh
+++ translate.sh
@@ -6,5 +6,9 @@ langs=( ja,ko,zh )
 for lang in "${langs[@]}"
 do
   echo 'Starting translation for $lang'
-  node index.js -s $1 -t $lang \;
+  git checkout -b "$lang-$1"
+  node index.js -s $1 -t "$lang" \;
+  git commit -m 'Addding $lang translation for $1'
+  git checkout master
+  git push origin "$lang-$1"
 done
