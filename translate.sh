a1fcdc36d8f18c29d06cfd7dc8c12b6b6748e08c
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 18:51:57 +0000

    Fixing bash script array itteration

--- translate.sh
+++ translate.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 export GOOGLE_APPLICATION_CREDENTIALS=../key.json
-langs=( ja,ko,zh )
+langs=( "ja" "ko" "zh" )
 
-for lang in "${langs[@]}"
+for language in "${langs[@]}"
 do
-  echo 'Starting translation for $lang'
-  git checkout -b "$lang-$1"
-  node index.js -s $1 -t "$lang" \;
-  git commit -m 'Addding $lang translation for $1'
+  echo "Starting translation for $language"
+  echo "Starting translation for $language"
+  git checkout -b "$language-$1"
+  node index.js -s $1 -t "$language" \;
+  git commit -m "Addding $language translation for $1"
   git checkout master
-  git push origin "$lang-$1"
+  git push origin "$language-$1"
 done
