a53a7f5a9f096135e8dccad3af6eb4a6f515373f
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 21:18:18 +0000

    Adding a better message

--- get-medium-post.sh
+++ get-medium-post.sh
@@ -12,5 +12,7 @@ npx mediumexporter $1 > ./docs/$2
 
 git add .
 git commit -m "Addding a new medium post as markdown from $1"
-$hub pull-request -p -m "Addding a new medium post as markdown frmo $1" -l 'New Original Post'
+$hub pull-request -p -m "Addding a new medium post as markdown from $1
+
+Please review it to make sure that the markdown renders correctly" -l 'New Original Post'
 git checkout master
