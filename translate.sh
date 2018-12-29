25cbe81878c5379f645d8617d33aa15c3fd9c814
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 18:41:48 +0000

    break the translation into individual langauges so that we can work out how to do manage git more effectively.

--- translate.sh
+++ translate.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 export GOOGLE_APPLICATION_CREDENTIALS=../key.json
+langs=( ja,ko,zh )
 
-node index.js -s $1 -t ko,ja,zh  \;
+for lang in "${langs[@]}"
+do
+  echo 'Starting translation for $lang'
+  node index.js -s $1 -t $lang \;
+done
