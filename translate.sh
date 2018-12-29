a7f18bc375d8449524dd44eb1c1d9d5772dfe5b4
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 19:34:19 +0000

    small fix.

--- translate.sh
+++ translate.sh
@@ -9,6 +9,7 @@ do
   echo "Starting translation for $language"
   git checkout -b "$language-$1"
   node index.js -s $1 -t "$language" \;
+  git add .
   git commit -m "Addding $language translation for $1"
   git push origin "$language-$1"
   hub pull-request -m "Addding $language translation for $1"
