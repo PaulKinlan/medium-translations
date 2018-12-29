95324f363e310eca09f67326e9ba827ca1e613e2
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 21:12:19 +0000

    adding import process

---
+++ get-medium-post.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# To run: ./get-medium-post.sh [url] [title.markdown]
+
+timestamp=(date +%s)
+
+echo "Exporting $1 to markdown"
+git checkout -b "new-medium-post-$timestamp"
+
+npx mediumexport $1 > ./docs/$2
+
+git add .
+git commit -m "Addding a new medium post as markdown from $1"
+$hub pull-request -p -m "Addding a new medium post as markdown frmo $1" -l 'New Original Post'
+git checkout master
