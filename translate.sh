0a15fc16bbf6a74c41cb66a579b31fc519cb7cf6
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 18:54:00 +0000

    clenaing up process in translation script.

--- translate.sh
+++ translate.sh
@@ -6,10 +6,9 @@ langs=( "ja" "ko" "zh" )
 for language in "${langs[@]}"
 do
   echo "Starting translation for $language"
-  echo "Starting translation for $language"
   git checkout -b "$language-$1"
   node index.js -s $1 -t "$language" \;
   git commit -m "Addding $language translation for $1"
-  git checkout master
   git push origin "$language-$1"
+  git checkout master
 done
