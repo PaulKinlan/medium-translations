3eb9615104a1494b463413a1566e13e1df171898
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-29 20:01:05 +0000

    resolveing path

--- translate.sh
+++ translate.sh
@@ -2,16 +2,16 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=../key.json
 langs=( "ja" "ko" "zh" )
+hub="binaries/linux-64/hub-linux-amd64-2.7.0/bin/hub"
 
 for language in "${langs[@]}"
 do
-  alias hub="binaries/linux-64/hub-linux-amd64-2.7.0/bin/hub"
   echo "Starting translation for $language"
   git checkout -b "$language-$1"
   node index.js -s $1 -t "$language" \;
   git add .
   git commit -m "Addding $language translation for $1"
   git push origin "$language-$1"
-  hub pull-request -m "Addding $language translation for $1"
+  $hub pull-request -m "Addding $language translation for $1"
   git checkout master
 done
