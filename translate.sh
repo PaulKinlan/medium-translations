4b8f34acf78d7e59c81a7539d1c2a989b2af7d6d
Paul Kinlan <paul.kinlan@gmail.com>
2018-12-30 13:20:17 +0000

    Fixes #32 - Adds langauge to PR - fixes previous issue

--- translate.sh
+++ translate.sh
@@ -12,6 +12,6 @@ do
   node index.js -s $1 -t "$language" \;
   git add .
   git commit -m "Addding $language translation for $1"
-  $hub pull-request -p -m "Addding $language translation for $1" --labels=translation
+  $hub pull-request -p -m "Addding $language translation for $1" --labels=translation,lang-$language
   git checkout master
 done
